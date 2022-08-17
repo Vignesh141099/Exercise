f21e58335be1bee670caa7e5f0e0f6d3b9426680
Vignesh141099 <110819624+Vignesh141099@users.noreply.github.com>
2022-08-17 12:29:26 +0530

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,7 @@
+n1= input("Enter the number:")
+n2 = input("Enter the number:")
+
+if n1>n2:
+  print(n1 is larger )
+else:
+  print(n2 is smaller)
---
+++ script2.py
@@ -0,0 +1,4 @@
+if n1<n2:
+  print(n1)
+else:
+  print(n2)
